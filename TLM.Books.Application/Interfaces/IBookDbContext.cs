823c37a16d65b29b4c2a2ab96fd1060be4823755
Ravi Le <ravi.le@transwap.com>
2022-05-22 23:35:16 +0700

    save code

---
+++ TLM.Books.Application/Interfaces/IBookDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TLM.Books.Domain.Entities;
+
+namespace TLM.Books.Application.Interfaces;
+
+public interface IBookDbContext
+{
+    DbSet<Book> Books { get; set; }
+    Task<int> SaveChangesAsync();
+}
---
+++ TLM.Books.Infrastructure/BookDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TLM.Books.Application.Interfaces;
+using TLM.Books.Domain.Entities;
+
+namespace TLM.Books._Infrastructure;
+
+public class BookDbContext: DbContext, IBookDbContext
+{
+    public BookDbContext(DbContextOptions<BookDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<Book> Books { get; set; }
+    public async Task<int> SaveChangesAsync()
+    {
+        return await base.SaveChangesAsync();
+    }
+}
